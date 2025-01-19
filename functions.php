d27620b37d580faeaada89b9b10175093b165f0e
fiske92 <fiske92@live.com>
2025-01-19 15:48:40 +0100

    Avoid loading all bootstrap components, to reduce css size

--- functions.php
+++ functions.php
@@ -16,19 +16,3 @@ require_once('vendor/autoload.php');
 use Boilerplate\ChildThemeSetup;
 
 new ChildThemeSetup();
-
-/**
- * Define Constants
- */
-define( 'CHILD_THEME_ALEKSAFISIC_VERSION', '1.0.0' );
-
-/**
- * Enqueue styles
- */
-function child_enqueue_styles() {
-
-	wp_enqueue_style( 'aleksafisic-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ALEKSAFISIC_VERSION, 'all' );
-
-}
-
-add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
